2f493355c066ff0714a8eb929b9f25261076d3d3
AmalAlby <amalalbyn@gmail.com>
2022-11-29 14:53:31 +0530

    Photosview created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ViewphotosComponent } from './viewphotos/viewphotos.component';
+
+
+const myroute:Routes=[
+  {
+    path:"",
+    component:ViewphotosComponent
+  }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ViewphotosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(myroute)
   ],
   providers: [],
   bootstrap: [AppComponent]
